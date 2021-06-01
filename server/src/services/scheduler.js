94046c66515fd39fd0941245ab7cb28262a78b97
williamstyronejr <itenory@gmail.com>
2021-06-01 12:49:59 -0400

    Created job for cleaning up rooms

--- server/src/services/scheduler.js
+++ server/src/services/scheduler.js
@@ -1,14 +1,34 @@
 const schedule = require('node-schedule');
+const logger = require('./logger');
 const { checkActiveQueues } = require('./challengeQueue');
+const { deleteCompletedRooms } = require('./room');
 
-let schedulerQueue;
+const { SERVER_INSTANCE_CID } = process.env;
+let schedulerQueue; // Job scheduler for queue challenges
+let schedulerRoom; // Job scheduler for room clean up
 
 /**
  * Sets up a scheduled job to run every 5 sceonds to check for active queues.
  */
 exports.setUpChallengeQueue = () => {
-  // Executes every 5 seconds
-  schedulerQueue = schedule.scheduleJob('*/5 * * * * *', checkActiveQueues);
+  if (SERVER_INSTANCE_CID === '0') {
+    schedulerQueue = schedule.scheduleJob('*/5 * * * * *', checkActiveQueues);
+  }
+};
+
+/**
+ * Sets up a scheduled job to remove rooms that are marked completed once a day.
+ */
+exports.setUpRoomCleanUp = () => {
+  if (SERVER_INSTANCE_CID === '0') {
+    schedulerRoom = schedule.scheduleJob('00 00 00 * * *', async () => {
+      try {
+        const results = await deleteCompletedRooms();
+      } catch (err) {
+        logger.error(err.stack);
+      }
+    });
+  }
 };
 
 /**
