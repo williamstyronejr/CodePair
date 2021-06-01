5a702046cd282d38373922e306cb9b2205cca372
williamstyronejr <itenory@gmail.com>
2021-05-31 21:26:16 -0400

    Updated github link for production

--- client/src/components/auth/GithubButton.js
+++ client/src/components/auth/GithubButton.js
@@ -3,11 +3,7 @@ import PropTypes from 'prop-types';
 
 const GithubButton = ({ signIn }) => (
   <div className="btn btn--social">
-    <a
-      className="github-link"
-      href="http://localhost:5000/auth/github/callback"
-      data-cy="github"
-    >
+    <a className="github-link" href="/auth/github/callback" data-cy="github">
       <span className="flex-wrapper flex-wrapper--center">
         <i className="github-logo devicon-github-plain-wordmark" />
         {signIn ? 'Sign in with Github' : 'Sign up with Github'}
