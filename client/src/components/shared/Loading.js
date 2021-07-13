8bdb5758470a48e11629f0e232c558730c65029d
williamstyronejr <itenory@gmail.com>
2021-07-12 22:07:25 -0400

    Updated loading wheel

--- client/src/components/shared/Loading.js
+++ client/src/components/shared/Loading.js
@@ -2,20 +2,32 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/loading.css';
 
-const LoadingComponent = ({ error }) => {
+const LoadingComponent = ({ message, error }) => {
   if (error) {
     return <div className=""> Error Loading. Please reload page</div>;
   }
 
-  return <div className="loader">Loading</div>;
+  return (
+    <div className="loading">
+      <div className="loading__dots">
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+      {message}
+    </div>
+  );
 };
 
 LoadingComponent.propTypes = {
   error: PropTypes.any,
+  message: PropTypes.string,
 };
 
 LoadingComponent.defaultProps = {
   error: null,
+  message: '',
 };
 
 export default LoadingComponent;
