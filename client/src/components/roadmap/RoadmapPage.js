e10b8c499c8b97f52723b2eb7813aed6d9547d76
williamstyronejr <itenory@gmail.com>
2021-07-27 19:23:37 -0400

    Add progress bar to roadmap

--- client/src/components/roadmap/RoadmapPage.js
+++ client/src/components/roadmap/RoadmapPage.js
@@ -5,6 +5,30 @@ const RoadmapPage = () => (
   <section className="roadmap">
     <header className="roadmap__header">
       <h2 className="roadmap__heading">Product Roadmap</h2>
+
+      <div>
+        <h4>In progress: User profile</h4>
+
+        <div>
+          <ol className="roadmap__progress-bar">
+            <li className="roadmap__step roadmap__step--complete">
+              <span className="roadmap__description" />
+            </li>
+            <li className="roadmap__step roadmap__step--complete">
+              <span className="roadmap__description">Design</span>
+            </li>
+            <li className="roadmap__step roadmap__step--active">
+              <span className="roadmap__description">Coding</span>
+            </li>
+            <li className="roadmap__step">
+              <span className="roadmap__description">Testing</span>
+            </li>
+            <li className="roadmap__step">
+              <span className="roadmap__description">Deploy</span>
+            </li>
+          </ol>
+        </div>
+      </div>
     </header>
 
     <div className="roadmap__content">
@@ -14,11 +38,16 @@ const RoadmapPage = () => (
         <ul className="roadmap__list">
           <li className="roadmap__item">
             <div className="roadmap__checkbox" />
-            <span className="roadmap__task">Darkmode</span>
+            <span className="roadmap__task">User Profile</span>
           </li>
           <li className="roadmap__item">
             <div className="roadmap__checkbox" />
-            <span className="roadmap__task">Profile</span>
+            <span className="roadmap__task">Ranking</span>
+          </li>
+
+          <li className="roadmap__item">
+            <div className="roadmap__checkbox" />
+            <span className="roadmap__task">User created challenges</span>
           </li>
         </ul>
       </div>
