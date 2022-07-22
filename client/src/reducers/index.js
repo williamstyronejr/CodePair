22982d48adf59e750c16520cbe083b8a12d38ef0
williamstyronejr <itenory@gmail.com>
2022-07-22 01:45:23 -0400

    Remove redux form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import userReducer from './userReducer';
 import challengeReducer from './challengeReducer';
 import chatReducer from './chatReducer';
@@ -7,7 +6,6 @@ import socketReducer from './socketReducer';
 import queueReducer from './queueReducer';
 
 export default combineReducers({
-  form: formReducer,
   user: userReducer,
   challenge: challengeReducer,
   chat: chatReducer,
