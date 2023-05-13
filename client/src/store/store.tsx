f4016604ec1480a4655e12b519712436d6060562
williamstyronejr <itenory@gmail.com>
2023-05-13 09:50:18 -0400

    Deleted ununsed files

--- client/src/store/store.tsx
+++
@@ -1,9 +0,0 @@
-// const { NODE_ENV } = process.env;
-import store from "./store.prod";
-
-// const store =
-// process.env.NODE_ENV === "development"
-//   ? require("./store.dev")
-//  require("./store.prod");
-
-export default store;
