d1102a454eb09e282b7fd1e119cdcaafe806b0f8
williamstyronejr <itenory@gmail.com>
2023-05-12 23:21:26 -0400

    Moved auth check

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,20 +1,7 @@
-import { ReactNode, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAppSelector, useAppDispatch } from '../hooks/reactRedux';
-import { fetchUserData } from '../reducers/userReducer';
+import { ReactNode } from 'react';
 import Header from './Header';
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
-  const user = useAppSelector((state) => state.user);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (!user.authenticated) dispatch(fetchUserData());
-  }, []);
-
-  if (user.authenticated && user.username)
-    return <Navigate replace to="/challenges" />;
-
   return (
     <>
       <Header />
