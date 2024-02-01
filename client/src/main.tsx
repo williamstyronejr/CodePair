86d71392341f4d9d308341712dbed8ddf70e5dba
williamstyronejr <itenory@gmail.com>
2024-02-01 10:12:49 -0500

    Removed redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,19 @@
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import Root from './pages/Root';
-import store from './store';
-import { setupSocketHandlers } from './middlewares/socket';
-import { fetchUserData } from './reducers/userReducer';
+import Providers from './Providers';
 import './styles/index.css';
 import 'normalize.css';
 
 // TODO: Dispatch only when there's a cookie present
-store.dispatch(fetchUserData());
+// store.dispatch(fetchUserData());
 
-setupSocketHandlers(store);
+// setupSocketHandlers(store);
 
 const container = document.getElementById('root');
 const root = createRoot(container as any);
+
 root.render(
-  <Provider store={store}>
+  <Providers>
     <Root />
-  </Provider>
+  </Providers>,
 );
