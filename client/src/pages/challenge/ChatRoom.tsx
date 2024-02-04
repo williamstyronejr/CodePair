e42b3033297359f09cea00282dd8bf1ad0b960a7
williamstyronejr <itenory@gmail.com>
2024-02-04 18:07:43 -0500

    Now handles typing indicator

--- client/src/pages/challenge/ChatRoom.tsx
+++ client/src/pages/challenge/ChatRoom.tsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect, useState, useCallback, KeyboardEvent } from 'react';
-import socket from '../../utils/socket';
+import socket, { emitChatTypingIndicator } from '../../utils/socket';
 import './styles/chatRoom.css';
 
 type ChatMessage = {
@@ -37,15 +37,19 @@ export default function ChatRoom({
   username,
   roomId,
   privateRoom,
+  initMessages,
 }: {
   userId: string;
   username: string;
   roomId: string;
   privateRoom: boolean;
+  initMessages: ChatMessage[];
 }) {
   const [active, setActive] = useState(false);
-  const [messages, setMessages] = useState<ChatMessage[]>([]);
+  const [messages, setMessages] = useState<ChatMessage[]>(initMessages);
+  const [usersTyping, setUsersTyping] = useState<string[]>([]);
   const [input, setInput] = useState('');
+  const chatRef = useRef<HTMLUListElement>(null);
 
   const addMessage = useCallback(
     ({ content, time, authorId, authorName }: ChatMessage) => {
@@ -76,6 +80,12 @@ export default function ChatRoom({
   );
 
   useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  useEffect(() => {
     socket.on('joinMessage', (username): void => {
       addMessage({
         authorId: 'notification',
@@ -98,12 +108,42 @@ export default function ChatRoom({
       addMessage({ authorId, authorName, time, content });
     });
 
+    socket.on('chatTyping', (username: string) => {
+      setUsersTyping((old) =>
+        old.includes(username) ? old : [...old, username]
+      );
+    });
+
+    socket.on('chatTypingFinish', (username: string) => {
+      setUsersTyping((old) => old.filter((val) => val !== username));
+    });
+
     return () => {
+      socket.off('chatTyping');
+      socket.off('chatTypingFinish');
       socket.off('joinMessage');
       socket.off('leaveMessage');
       socket.off('receiveMessage');
     };
   }, [privateRoom, addMessage]);
+
+  useEffect(() => {
+    let typingTimeout: NodeJS.Timeout | null = null;
+
+    if (input !== '') {
+      emitChatTypingIndicator(true, username, roomId);
+
+      typingTimeout = setTimeout(() => {
+        if (input !== '') emitChatTypingIndicator(false, username, roomId);
+      }, 5000);
+    } else {
+      emitChatTypingIndicator(false, username, roomId);
+    }
+
+    return () => {
+      if (typingTimeout) clearTimeout(typingTimeout);
+    };
+  }, [input, username, roomId]);
 
   if (privateRoom) return null;
 
@@ -136,7 +176,7 @@ export default function ChatRoom({
         </h4>
       </header>
 
-      <ul className="chat__messages">
+      <ul className="chat__messages" ref={chatRef}>
         {messages.map((msg, index) => {
           let msgType = msg.authorId === userId ? 'client' : 'server';
           if (msg.authorId === 'notification') msgType = 'notification';
@@ -153,6 +193,22 @@ export default function ChatRoom({
             </li>
           );
         })}
+
+        {usersTyping.map((user, index) => (
+          <li
+            key={`typing-${username}-${index}`}
+            className="message message--typing"
+          >
+            <div className="chat__content--typing">
+              <div className="chat__dots">
+                <div className="chat__dot chat__dot--first" />
+                <div className="chat__dot chat__dot--second" />
+                <div className="chat__dot chat__dot--third" />
+              </div>
+            </div>
+            <p className="content__text chat__user">{user}</p>
+          </li>
+        ))}
       </ul>
 
       <div className="chat__input">
@@ -168,140 +224,3 @@ export default function ChatRoom({
     </div>
   );
 }
-
-// const ChatRoom = ({
-//   userId,
-//   setMessage,
-//   sendMessage,
-//   visible,
-//   toggleChat,
-//   chatInput,
-//   messages,
-//   usersTyping,
-//   messageIndicator,
-// }: {
-//   userId: string;
-//   setMessage: any;
-//   sendMessage: any;
-//   visible: any;
-//   toggleChat: any;
-//   chatInput: any;
-//   messages: any;
-//   usersTyping: string[];
-//   messageIndicator: any;
-// }) => {
-//   const chatRef = useRef<HTMLUListElement>(null);
-
-//   useEffect(() => {
-//     if (chatRef.current)
-//       chatRef.current.scrollTop = chatRef.current.scrollHeight;
-//   }, [messages]);
-
-//   useEffect(() => {
-//     if (chatInput !== '') {
-//       messageIndicator(true);
-//     } else {
-//       messageIndicator(false);
-//     }
-
-//     const typingTmeout = setTimeout(() => {
-//       if (chatInput !== '') {
-//         messageIndicator(false);
-//       }
-//     }, 5000);
-
-//     return () => {
-//       if (typingTmeout) clearTimeout(typingTmeout);
-//     };
-//   }, [chatInput]);
-
-//   if (visible) {
-//     return (
-//       <div className="chat chat--hidden" data-cy="chat">
-//         <button className="btn" type="button" onClick={() => toggleChat()}>
-//           <i className="fas fa-comment chat__icon" />
-//         </button>
-//       </div>
-//     );
-//   }
-
-//   const onChange = (e: any) => {
-//     setMessage(e.target.value);
-//   };
-
-//   // When user hits enter (without shift), send current message
-//   const onKeyDown = (event: any) => {
-//     if (event.keyCode === 13 && !event.shiftKey) {
-//       event.preventDefault(); // Stop '\n' from being put in form
-
-//       // Prevent sending empty message (spaces work)
-//       if (chatInput) sendMessage(chatInput);
-//     }
-//   };
-
-//   const messageList = messages.map((msg: any) => {
-//     const type =
-//       msg.author === userId
-//         ? 'client'
-//         : msg.author === 'notification'
-//         ? 'notification'
-//         : 'server';
-
-//     return (
-//       <li
-//         className={`message message-${type}`}
-//         key={`${msg.author}-${msg.time}-${type}`}
-//         data-cy="message"
-//       >
-//         <div className={`message-${type}__content`}>
-//           <p className="content__text">{msg.content}</p>
-//         </div>
-//       </li>
-//     );
-//   });
-
-//   return (
-//     <div className="chat chat--active" data-cy="chat">
-//       <header className="chat__header">
-//         <h4 className="chat__heading">
-//           <button className="btn btn--chat" type="button" onClick={toggleChat}>
-//             Chat
-//           </button>
-//         </h4>
-//       </header>
-
-//       <ul className="chat__messages" ref={chatRef}>
-//         {messageList}
-
-//         {usersTyping.map((username, index) => (
-//           <li
-//             key={`typing-${username}-${index}`}
-//             className="message message--typing"
-//           >
-//             <div className="chat__content--typing">
-//               <div className="chat__dots">
-//                 <div className="chat__dot chat__dot--first" />
-//                 <div className="chat__dot chat__dot--second" />
-//                 <div className="chat__dot chat__dot--third" />
-//               </div>
-//             </div>
-//             <p className="content__text chat__user">{username}</p>
-//           </li>
-//         ))}
-//       </ul>
-
-//       <div className="chat__input">
-//         <textarea
-//           className="input__text"
-//           data-cy="chatInput"
-//           value={chatInput}
-//           onChange={onChange}
-//           onKeyDown={onKeyDown}
-//           placeholder="Type a message ..."
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ChatRoom;
