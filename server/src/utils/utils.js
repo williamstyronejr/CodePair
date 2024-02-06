04404bf51a5fdfd27364449e8e511ae41f9dbef8
williamstyronejr <itenory@gmail.com>
2024-02-06 18:27:22 -0500

    Create function to standardized pending key

--- server/src/utils/utils.js
+++ server/src/utils/utils.js
@@ -9,7 +9,17 @@ exports.createRandomString = (length, append = '') => {
   do {
     s += Math.random().toString(36).substr(2);
   } while (s.length < length);
-  s = s.substr(0, length);
+  s = s.substring(0, length);
 
   return s.concat(append);
+};
+
+/**
+ * Creates a key for a pending match in redis. Key will always end with
+ *  "-(queueid)".
+ * @param {String} queueId Id of match queue to create pending key for
+ * @returns {String} Returns a random string to be used as pendingKey for redis.
+ */
+exports.createPendingKey = (queueId) => {
+  return `${this.createRandomString(12)}-${queueId}`;
 };
