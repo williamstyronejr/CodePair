a52641003d1c9295919f12dd1064e12d12f65a8b
williamstyronejr <itenory@gmail.com>
2024-04-03 20:19:15 -0400

    Fixed format of output to match nodejs version

--- launcher/src/services/testRunners/python/testingLib.py
+++ launcher/src/services/testRunners/python/testingLib.py
@@ -21,38 +21,38 @@ class Expect():
     global testsFailed
     # Receiving function as expectedvalue will be marked as failed
     if callable(expectedValue):
-      expectedValue[self.id] = {
-        "name": self.id,
-        "expects": {
+      expectations[self.id]["expects"] = [
+        *expectations[self.id]["expects"],
+        {
           "status": False,
           "name": f"Expected: {expectedValue} Received: function"
         }
-      }
+      ]
       testsFailed += 1
       return
 
 
     # Dict comparison
     if (self.value == expectedValue):
-      expectations[self.id] = {
-        "name": self.id,
-        "expects": {
+      expectations[self.id]["expects"] = [
+        *expectations[self.id]["expects"],
+        {
           "status": True,
           "name": f"Expected: {expectedValue} Received: {self.value}"
         }
-      }
+      ]
 
       testsPassed += 1
       return
 
 
-    expectations[self.id]= {
-        "name": self.id,
-        "expects": {
-          "status": False,
-          "name": f"Expected: {expectedValue} Received: {self.value}"
-        }
-    }
+    expectations[self.id]["expects"] = [
+      *expectations[self.id]["expects"],
+      {
+        "status": False,
+        "name": f"Expected: {expectedValue} Received: {self.value}"
+      }
+    ]
     testsFailed += 1
 
 
@@ -67,15 +67,19 @@ def test(name, fn, timeout = 5000):
 
 def runTestFile():
   for userTest in tests:
-    expectations[userTest["name"]] = { "status": True, "expects": [] }
+    expectations[userTest["name"]] = { "name": userTest["name"], "status": True, "expects": [] }
     userTest["fn"](userTest["name"])
+    for expect in expectations[userTest["name"]]["expects"]:
+       if not expect["status"]:
+          expectations[userTest["name"]]["status"] = False
+          break
 
 def showResults():
 	output = {
 		"numOfTests": totalTest,
 		"passedTests": testsPassed,
 		"failedTests": testsFailed,
-		"testResults": expectations,
+		"testResults": list(expectations.values()),
 		"time": 0,
 	}
 
